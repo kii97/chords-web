ebf7a996c45fda9a145a7fbd4eaec7b688b1c8d4
ZQW <493908322@qq.com>
2026-06-06 19:03:21 +0800

    配置和弦部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/chords/",
   server: {
     host: "127.0.0.1",
     port: 5173,
